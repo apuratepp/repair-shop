da68e95b9f2cbc5245ee54d8f8f3a26e8b6244dc
Josep <apuratepp@gmail.com>
2012-12-16 19:02:25 +0100

    service model

---
+++ app/models/service.rb
@@ -0,0 +1,3 @@
+class Service < ActiveRecord::Base
+  attr_accessible :client_id, :delivered_at, :delivery_estimated_at, :description, :email, :kind_id, :last_name, :name, :ordered_at, :phone, :reference
+end
