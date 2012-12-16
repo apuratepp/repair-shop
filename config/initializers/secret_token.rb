1b69344a8dde4369e8d6e4c4089c394a02e17996
Josep <apuratepp@gmail.com>
2012-12-16 18:48:03 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RepairShop::Application.config.secret_token = 'f8269b82057453c7531622af263f0ba506e3229729b80fc690b0786c65f2380507b4303354dc98ae0560b5f7eed4d64086096a02473ee879d570e671343679d9'
