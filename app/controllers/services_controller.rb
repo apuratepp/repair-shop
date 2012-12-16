47320ec658e718fe7cf5c4b8bfcdfd714ddd1ada
Josep <apuratepp@gmail.com>
2012-12-16 19:15:04 +0100

    form services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,30 @@
 class ServicesController < ApplicationController
+  def index
+    
+  end
+  def show
+    @service = Service.find(params[:id])
+  end
+  def new
+    @service = Service.new
+  end
+  def create
+    @service = Service.new(params[:service])
+    if @service.save
+      
+    end
+  end
+  def edit
+    @service = Service.find(params[:id])
+  end
+  def update
+    @service = Service.find(params[:id])
+    if @service.update_attributes(params[:service])
+    
+    end
+  end
+  def destroy
+    @service = Service.find(params[:id])
+    @service.destroy
+  end
 end
