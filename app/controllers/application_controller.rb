8883a55182971c42f79b8a41530ac9917310c6ae
Josep <apuratepp@gmail.com>
2012-12-17 08:33:38 +0100

    proves amb headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+  #before_filter :allow_cross_domain_access
+  
+  def allow_cross_domain_access
+    headers["Access-Control-Allow-Origin"] = "*"
+    headers["Access-Control-Allow-Methods"] = "*"
+    headers['Access-Control-Allow-Headers'] = 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  end
 end
