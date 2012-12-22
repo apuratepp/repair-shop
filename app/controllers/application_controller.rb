23441b7170ecdc208948b5dc8450739b412e9511
Josep <apuratepp@gmail.com>
2012-12-22 12:48:11 +0100

    afegit callback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,9 @@ class ApplicationController < ActionController::Base
   #before_filter :authenticate_user!
   #before_filter :allow_cross_domain_access
   
-  ActiveRecord::Base.include_root_in_json = false
-  
-  def allow_cross_domain_access
-    headers["Access-Control-Allow-Origin"] = "*"
-    headers["Access-Control-Allow-Methods"] = "*"
-    headers['Access-Control-Allow-Headers'] = 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  end
+  # def allow_cross_domain_access
+  #   headers["Access-Control-Allow-Origin"] = "*"
+  #   headers["Access-Control-Allow-Methods"] = "*"
+  #   headers['Access-Control-Allow-Headers'] = 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  # end
 end
